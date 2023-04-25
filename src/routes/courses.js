7b4c3bdd0a814d39671a068568cf6b987e5e1a21
ddoyediran <ddoyediran@gmail.com>
2023-04-26 01:08:44 +0200

    Get all courses controller and route added

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -8,9 +8,9 @@ const {
 const { isTokenValid } = require("../utils/jwt");
 const router = express.Router();
 
-router.get("/courses", getAllCourses);
-router.get("/course/:id", isTokenValid, getCourse);
-router.post("/course/create", isTokenValid, createCourse);
-router.put("/course/update/:id", updateCourse);
+router.get("/courses", isTokenValid, getAllCourses);
+router.get("/courses/:id", isTokenValid, getCourse);
+router.post("/courses/create", isTokenValid, createCourse);
+router.put("/courses/update/:id", updateCourse);
 
 module.exports = router;
